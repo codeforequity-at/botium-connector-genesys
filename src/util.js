9ee5f2d5ece94d46d6e7a7e3400334c86f8afdf6
Szabolcs Gergye <szabolcs.gergye@cyara.com>
2024-12-19 11:06:50 +0100

    BOT-3581 SNYK: Upgrade "request" library to node-native "fetch"

--- src/util.js
+++ src/util.js
@@ -1,22 +1,30 @@
 const { UrlsByRegion } = require('./constants')
-const qs = require('qs')
-const axios = require('axios')
 const _ = require('lodash')
 
 const getAccessToken = async (awsRegion, clientId, clientSecret) => {
   try {
     const authEndpoint = _.get(UrlsByRegion, `${awsRegion}.auth`)
     const requestOptions = {
-      method: 'post',
       url: `${authEndpoint}/oauth/token`,
+      method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`
       },
-      data: qs.stringify({ grant_type: 'client_credentials' })
+      body: JSON.stringify({ grant_type: 'client_credentials' })
     }
-    const authResult = await axios(requestOptions)
-    return authResult.data.access_token
+    const authResponse = await fetch(requestOptions.url, {
+      method: requestOptions.method,
+      headers: requestOptions.headers,
+      body: requestOptions.body
+    })
+
+    if (!authResponse.ok) {
+      const errorDetails = await authResponse.text()
+      throw new Error(`HTTP error! Status: ${authResponse.status}, Message: ${errorDetails}`)
+    }
+    const authResult = await authResponse.json()
+    return authResult.access_token
   } catch (err) {
     throw new Error(`Failed to get access token: ${err.message}`)
   }
